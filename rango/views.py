3967f94045bf0761529e635b153cb3765e9b047f
Harry Lynn <2759078L@student.gla.ac.uk>
2024-02-07 21:01:43 +0000

    Completed Chapter 3 Exercises

---
+++ rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("""<a href='/rango/about/'>About</a><br>
+    Rango says hey there partner!""")
+
+def about(request):
+    return HttpResponse("""<a href='/rango/'>Index</a><br>
+    Rango says here is the about page.""")
